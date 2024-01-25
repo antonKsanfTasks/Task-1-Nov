5d7a2d9986d19fbc898990f7256cce706a5efbed
antonksanf <antonksanf@gmail.com>
2024-01-25 21:39:33 +0300

    JDBC

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    private final static String dbDriver = "org.postgresql.Driver";
+    private final static String  dbURL = "jdbc:postgresql://localhost:5432/postgres";
+    private final static String  dbUser = "postgres";
+    private final static String  dbPass = "1234";
+
+    public static Connection openConnection() throws SQLException {
+        try {
+            Class.forName(dbDriver);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(dbURL, dbUser, dbPass);
+    }
 }
